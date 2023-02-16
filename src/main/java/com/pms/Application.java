49eadfff5a22950dc4a439abbe81f4fdb9474466
liangrj <huangyan20190914@163.com>
2023-02-16 14:49:56 +0800

    创建启动类及核心配置文件

---
+++ src/main/java/com/pms/Application.java
@@ -0,0 +1,16 @@
+package com.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author liangrj
+ * @since 2023/02/16
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
