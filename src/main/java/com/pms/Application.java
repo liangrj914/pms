afd34b24187e9ba99b0a4b40158976cc9a3adc12
liangrj <huangyan20190914@163.com>
2023-03-01 22:33:49 +0800

    添加密码加密组件依赖

--- src/main/java/com/pms/Application.java
+++ src/main/java/com/pms/Application.java
@@ -3,6 +3,7 @@ package com.pms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author liangrj
  * @since 2023/02/16
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableSwagger2
 @EnableOpenApi
 @MapperScan(basePackages = {"com.pms.system.mapper"})
